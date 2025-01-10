3818a5962e87a40b6ad190766db9f1cba779035b
Gellértfy "Huto-GITANES" Tamás <82087132+LiLHuto@users.noreply.github.com>
2025-01-10 10:15:21 +0100

    zsa

--- gipowinery oldal/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Ha nincs bejelentkezve a felhasználó, átirányítjuk a login oldalra
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
-?>
