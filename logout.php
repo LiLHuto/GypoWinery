85ff37231d79cd3aa7e0c5d3f8bdc0289ad01159
Gellértfy "Huto-GITANES" Tamás <82087132+LiLHuto@users.noreply.github.com>
2025-02-05 13:06:08 +0100

    mappák szét pukkadtad de elv jó

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();  // Session változók törlése
-session_destroy();  // Session megsemmisítése
-header("Location: index.php");  // Vissza a főoldalra
-exit();
-?>
--- user-menu.js
+++
@@ -1,4 +0,0 @@
-function toggleMenu() {
-    var dropdown = document.getElementById("userDropdown");
-    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
-}
